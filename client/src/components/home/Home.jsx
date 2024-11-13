fd50575c0d606c0faa458b20defebf111088300f
TeodorIliev06 <tedo2505@gmail.com>
2024-11-13 22:15:08 +0200

    Added listing games and details functionality

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+import gamesAPI from "../../api/games-api";
+import LatestGame from "./latest-game/LatestGame";
+
 export default function Home() {
+    const [latestGames, setLatestGames] = useState([]);
+    useEffect(() => {
+        (async () => {
+
+            //TODO: modify to fetch lastest
+            const result = await gamesAPI.getAll();
+
+            setLatestGames(result);
+        })();
+    }, []);
+
     return (
         <section id="welcome-world">
             <div className="welcome-message">
@@ -8,61 +23,10 @@ export default function Home() {
             <img src="./images/four_slider_img01.png" alt="hero" />
             <div id="home-page">
                 <h1>Latest Games</h1>
-                <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/CoverFire.png" />
-                    </div>
-                    <h3>Cover Fire</h3>
-                    <div className="rating">
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/ZombieLang.png" />
-                    </div>
-                    <h3>Zombie Lang</h3>
-                    <div className="rating">
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/MineCraft.png" />
-                    </div>
-                    <h3>MineCraft</h3>
-                    <div className="rating">
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <p className="no-articles">No games yet</p>
+                {latestGames.length > 0
+                    ? latestGames.map(game => <LatestGame key={game._id} {...game}/>)
+                    : <p className="no-articles">No games yet</p>
+                }
             </div>
         </section>
     );
