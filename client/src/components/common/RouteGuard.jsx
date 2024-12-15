7a762170cb31537831c384f34327f586711447f4
TeodorIliev06 <tedo2505@gmail.com>
2024-12-15 23:47:32 +0200

    Add route guards

---
+++ client/src/components/common/RouteGuard.jsx
@@ -0,0 +1,14 @@
+import { Navigate, useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../contexts/AuthContext";
+
+export default function RouteGuard({children}) {
+    const{isAuthenticated}=useAuthContext();
+    if(!isAuthenticated){
+        return <Navigate to='/login'/>
+    }
+  return (
+    <>
+{children}
+    </>
+  );
+}
